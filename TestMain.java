e0f01a6682987a405452a5552e0b07517d33188a
Dave Taylor <davehtaylor@me.com>
2018-01-22 17:59:18 -0500

    Cleaning up main a bit more

--- TestMain.java
+++ TestMain.java
@@ -4,10 +4,23 @@ public class TestMain
 {
     public static void printMainMenu()
     {
+        Scanner input = new Scanner(System.in);
+        int mainMenuChoice;
+
         System.out.printf("***** GAME TIME *****%n%n");
         System.out.printf("1. Play Game%n");
         System.out.printf("2. Exit%n%n");
         System.out.printf("*********************%n%n");
+
+        System.out.printf("Your selection: ");
+
+        mainMenuChoice = input.nextInt();
+        input.nextLine();
+
+        if (mainMenuChoice != 1)
+            System.exit(0);
+        else
+            return;
     }
 
 
@@ -18,6 +31,8 @@ public class TestMain
 
         Scanner input = new Scanner(System.in);
         String name;
+
+        System.out.printf("%n*** Create your character ***%n%n");
 
         System.out.printf("Name: ");
         name = input.nextLine();
@@ -30,46 +45,28 @@ public class TestMain
 
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        int mainMenuChoice;
         Player Player1 = null;
         Item newItem = null;
         Item newItem2 = null;
 
         // Print main menu
         printMainMenu();
+
+        // Create character
+        Player1 = characterCreation(Player1);
         
-        System.out.printf("Your selection: ");
+        // Adding items to test things out
+        newItem = new Item("Computer", 5, 100);
+        newItem2 = new Item("Flashlight", 1, 5);
+        Player1.addItem(newItem);
+        Player1.addItem(newItem2);
 
-        mainMenuChoice = input.nextInt();
-        input.nextLine();
-
-        if (mainMenuChoice != 1)
-        {
-            System.out.println("Goodbye");
-            return;
-        }
-        else
-        {
-            System.out.printf("%n*** Create your character ***%n%n");
-
-            // Create character
-            Player1 = characterCreation(Player1);
-            
-            // Adding items to test things out
-            newItem = new Item("Computer", 5, 100);
-            newItem2 = new Item("Flashlight", 1, 5);
-            Player1.addItem(newItem);
-            Player1.addItem(newItem2);
-
-            System.out.printf("%nWelcome, %s%n", Player1.getName());
-            System.out.printf("Current HP: %d/%d%n",
-                    Player1.getCurrentHP(), Player1.getMaxHP());
-            System.out.printf("Current XP: %d%n", Player1.getCurrentXP());
-            System.out.printf("Current level: %d%n", Player1.getCurrentLevel());
-            System.out.println("Current items:");
-            Player1.listItems();
-            
-        }
+        System.out.printf("%nWelcome, %s%n", Player1.getName());
+        System.out.printf("Current HP: %d/%d%n",
+                Player1.getCurrentHP(), Player1.getMaxHP());
+        System.out.printf("Current XP: %d%n", Player1.getCurrentXP());
+        System.out.printf("Current level: %d%n", Player1.getCurrentLevel());
+        System.out.println("Current items:");
+        Player1.listItems();
     }
 }
