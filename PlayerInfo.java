92a7626c2379a2efaeff1ee0eb481b7d0ac28753
Dave Taylor <davehtaylor@me.com>
2018-01-29 15:05:56 -0500

    Added PlayerInfo enum

---
+++ PlayerInfo.java
@@ -0,0 +1,35 @@
+// We'll use this for input validation, as well as
+// for condensing getters and setters for the player
+// stats. 
+public enum PlayerInfo
+{
+    // Basic player info
+    NAME,
+    CURRENTHP,
+    MAXHP,
+    CURRENTXP,
+    CURRENTLEVEL,
+    CURRENTCARRYWEIGHT,
+    MAXCARRYWEIGHT,
+
+    // Attributes
+    CHARISMA,
+    CONSTITUTION,
+    DEXTERITY,
+    INTELLIGENCE,
+    STRENGTH,
+    WISDOM,
+
+    // Skills
+    ACROBATICS,
+    ATHLETICS,
+    COMPUTERSCIENCE,
+    ELECTRONICS,
+    INSIGHT,
+    INTIMIDATION,
+    INVESTIGATION,
+    PERCEPTION,
+    PERSUASION,
+    STEALTH,
+    SURVIVAL
+}
